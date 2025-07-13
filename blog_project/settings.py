98b6aab9614e2499ccea3710a8bd3ef4a4b3f76f
selwyn427 <surng123@gmail.com>
2025-07-13 21:23:28 +1000

    Fix static files configuration - update STATIC_ROOT to match docker volume mount

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -135,9 +135,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'staticfiles'),
+    os.path.join(BASE_DIR, 'static'),
 ]
 
 # 配置媒体文件
